7174178a8ff1873c78fedd08e0ed1c2b1d462d9b
Luke Parker <lukeparker5132@gmail.com>
2025-09-01 19:08:28 -0400

    Update documentation

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
 # serai-polkadot-sdk
 
-A binary to generate the `polkadot-sdk` in the optimal form for Serai.
+A script to generate the `polkadot-sdk` in the optimal form for Serai.
+
+# Details
+
+The script, `patch.sh`, makes uses of a Rust binary (`serai-polkadot-sdk`), to
+accomplish its goal. Functionality is split between the two of them. The
+`patches` folder contains a set of patches we apply to `polkadot-sdk`. Note
+that not all changes are accomplished via the `patches` folder, and some
+modifications are inlined into the `patch.sh` script.
+
+THe primary goal is simply to minimize the `polkadot-sdk` tree. This script
+more than halves the amount of dependencies in use, and the result still takes
+tens of minutes to be checked, producing a target exceeding 10 GB
+_without producing any binaries_.
+
+While removals may be over-eager, the point is removed crates may be quickly
+restored as needed, if needed. Accordingly, there's no harm to being aggressive
+now.
+
+# Licensing
 
 The `polkadot-sdk` folder included in this repository is the artifact output
-from running the binary. It is a `git clone` of the `paritytech/polkadot-sdk`
+from running the script. It is a `git clone` of the `paritytech/polkadot-sdk`
 repository, with many removals and light patches applied. `polkadot-sdk`, as
 originally cloned, is as licensed by Parity (with their license and copyright
 statements fully intact, preserved in the derivative produced). The patches
@@ -12,3 +31,6 @@ present in `patches`, the transformations of `serai-polkadot-sdk` and
 under the AGPL 3.0 only, with no grant to use future revisions of the AGPL
 license. A copy of the AGPL and short license texts are included within this
 repository.
+
+The `.github` folder is published under the MIT license. Please see
+`.github/LICENSE` for more information.
--- patch.sh
+++ patch.sh
@@ -1,3 +1,5 @@
+# Start by checking out the desired version of the polkadot-sdk
+
 POLKADOT_SDK_COMMIT=52f4a08f26f226de93c0dbea5e8d066cbbd5bbd0
 
 if [ -f "polkadot-sdk/.patched" ]; then
@@ -33,36 +35,9 @@ git checkout -f $POLKADOT_SDK_COMMIT
 rm .patched
 cd ..
 
-function remove_crate_tree {
-  echo "Removing crates $1"
-  cargo run --release -- remove_crate_tree $1
-  if [ $? -ne 0 ]; then
-    exit 2
-  fi
-}
-
-function remove_workspace_dependency {
-  echo "Removing workspace dependency $1"
-  cargo run --release -- remove_workspace_dependency $1
-  if [ $? -ne 0 ]; then
-    exit 2
-  fi
-}
-
-function remove_feature {
-  echo "Removing feature $1"
-  cargo run --release -- remove_feature $1
-  if [ $? -ne 0 ]; then
-    exit 2
-  fi
-}
-
-function remove_dev_dependencies {
-  cargo run --release -- remove_dev_dependencies
-  if [ $? -ne 0 ]; then
-    exit 2
-  fi
-}
+# Our binary, when it makes its modifications, will overwrite all existing
+# `Cargo.toml` files until they're unrecognizable. We start by making changes
+# _to_ the `Cargo.toml` files accordingly
 
 function apply_patch {
   cd polkadot-sdk
@@ -74,75 +49,98 @@ function apply_patch {
   fi
 }
 
-# Apply the `wasmtime` patch BEFORE we re-generate every crate's `Cargo.toml`, due to tweaking features
+function remove_matching_lines {
+  # This uses the outer `echo` as a buffer so we don't immediately overwrite
+  # the file we're reading from
+  echo "$(cat $1 | grep -v $2)" > $1
+}
+
+# Apply the `wasmtime` patch, which will update the features within the `Cargo.toml`s
 apply_patch patches/wasmtime.patch
-# The same for twox-hash
+# The same for `twox-hash`
 apply_patch patches/twox-hash.patch
+
 # Remove some unused dependencies
-echo "$(cat ./polkadot-sdk/substrate/client/network/Cargo.toml | grep -v 'cid')" > ./polkadot-sdk/substrate/client/network/Cargo.toml
-echo "$(cat ./polkadot-sdk/substrate/client/network/Cargo.toml | grep -v 'prost')" > ./polkadot-sdk/substrate/client/network/Cargo.toml
-echo "$(cat ./polkadot-sdk/substrate/primitives/core/Cargo.toml | grep -v 'ark-vrf')" > ./polkadot-sdk/substrate/primitives/core/Cargo.toml
-echo "$(cat ./polkadot-sdk/substrate/primitives/core/Cargo.toml | grep -v 'w3f-bls')" > ./polkadot-sdk/substrate/primitives/core/Cargo.toml
-echo "$(cat ./polkadot-sdk/substrate/primitives/weights/Cargo.toml | grep -v 'schemars')" > ./polkadot-sdk/substrate/primitives/weights/Cargo.toml
-echo "$(cat ./polkadot-sdk/substrate/utils/wasm-builder/Cargo.toml | grep -v 'merkleized-metadata')" > ./polkadot-sdk/substrate/utils/wasm-builder/Cargo.toml
+remove_matching_lines ./polkadot-sdk/substrate/client/network/Cargo.toml "cid"
+remove_matching_lines ./polkadot-sdk/substrate/client/network/Cargo.toml "prost"
+remove_matching_lines ./polkadot-sdk/substrate/primitives/core/Cargo.toml "ark-vrf"
+remove_matching_lines ./polkadot-sdk/substrate/primitives/core/Cargo.toml "w3f-bls"
+remove_matching_lines ./polkadot-sdk/substrate/primitives/weights/Cargo.toml "schemars"
+remove_matching_lines ./polkadot-sdk/substrate/utils/wasm-builder/Cargo.toml "merkleized-metadata"
 # Also prune dated workspace dependency specifications we don't need
 # This lets us detect which ones are actually worth upgrading
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^alloy-core')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^always-assert')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^ark-')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^bincode')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^bounded-vec')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^cid')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^cmd_lib')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^colored')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^criterion')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^gethostname')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^handlebars')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^jemalloc_pprof')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^landlock')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^merkleized-metadata')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^nix')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^procfs')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^quick_cache')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^rstest')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^schemars')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^seccompiler')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^serde-big-array')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^smoldot')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^subxt')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^sysinfo')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^tikv')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^tokio-tungstenite')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^trie-bench')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^wasmi')" > ./polkadot-sdk/Cargo.toml
-echo "$(cat ./polkadot-sdk/Cargo.toml | grep -v '^zombienet')" > ./polkadot-sdk/Cargo.toml
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^alloy-core"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^always-assert"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^ark-"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^bincode"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^bounded-vec"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^cid"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^cmd_lib"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^colored"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^criterion"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^gethostname"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^handlebars"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^jemalloc_pprof"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^landlock"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^merkleized-metadata"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^nix"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^procfs"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^quick_cache"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^rstest"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^schemars"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^seccompiler"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^serde-big-array"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^smoldot"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^subxt"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^sysinfo"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^tikv"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^tokio-tungstenite"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^trie-bench"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^wasmi"
+remove_matching_lines ./polkadot-sdk/Cargo.toml "^zombienet"
 
+# Remove the `bridges/` tree, as we won't use it
 remove_crate_tree bridges
-# Cleanup 'snowbridge' dependency not removed with `bridges/`
+# Cleanup "snowbridge" dependency not removed with `bridges/`
 remove_workspace_dependency milagro-bls
 
+# Remove the `cumulus/` tree, intended for parachains
 remove_crate_tree cumulus
 
+# Remove the `polkadot/` tree, intended for Polkadot
 remove_crate_tree polkadot
 
+# Remove the `docs/sdk` crate, which won't compile after this and isn't worth
+# the effort to patch
 remove_crate_tree docs/sdk
 
+# Remove the `umbrella` crate, which we don't use, so we don't have to
+# re-generate it (requiring multiple bespoke binary tools be added to the system
+# running this script)
 remove_crate_tree umbrella
 
+# Remove the `templates` folder (effectively examples)
 remove_crate_tree templates
+# And the actual examples
+remove_crate_tree substrate/frame/examples
 
+# Remove the deprecated crates
 remove_crate_tree substrate/deprecated
 
+# Remove the "kitchensink" runtime, which has everything including the kitchen
+# sink, as we remove most things
 remove_crate_tree substrate/utils/frame/generate-bags/node-runtime
 remove_crate_tree substrate/test-utils/cli
 remove_crate_tree substrate/bin/node
 
+# Remove the unused "bitswap" protocol
 rm polkadot-sdk/substrate/client/network/build.rs
 rm -rf polkadot-sdk/substrate/client/network/src/bitswap
 rm polkadot-sdk/substrate/client/network/src/litep2p/shim/bitswap.rs
 rm polkadot-sdk/substrate/client/network/src/schema/bitswap.v1.2.0.proto
 apply_patch patches/remove_bitswap.rs
 
+# Remove the BEEFY consensus crates
 remove_crate_tree substrate/client/consensus/beefy
 remove_crate_tree substrate/client/merkle-mountain-range
 remove_crate_tree substrate/frame/beefy
@@ -151,44 +149,51 @@ remove_crate_tree substrate/frame/merkle-mountain-range
 remove_crate_tree substrate/primitives/consensus/beefy
 remove_crate_tree substrate/primitives/merkle-mountain-range
 
+# Remove the SASSAFRAS consensus crates
 remove_crate_tree substrate/frame/sassafras
 remove_crate_tree substrate/primitives/consensus/sassafras
 
+# Remove the proof-of-work consensus crates
 remove_crate_tree substrate/client/consensus/pow
 remove_crate_tree substrate/primitives/consensus/pow
 
 remove_crate_tree substrate/frame/contracts
 remove_crate_tree substrate/frame/revive
 
+# Remove the mixnet code
 remove_crate_tree substrate/client/mixnet
 remove_crate_tree substrate/frame/mixnet
 remove_crate_tree substrate/primitives/mixnet
 rm -rf ./polkadot-sdk/substrate/client/rpc-api/src/mixnet
-echo "$(cat ./polkadot-sdk/substrate/client/rpc-api/src/lib.rs | grep -v "mod mixnet;$")" > ./polkadot-sdk/substrate/client/rpc-api/src/lib.rs
+remove_matching_lines ./polkadot-sdk/substrate/client/rpc-api/src/lib.rs "mod mixnet;$"
 rm -rf ./polkadot-sdk/substrate/client/rpc/src/mixnet
-echo "$(cat ./polkadot-sdk/substrate/client/rpc/src/lib.rs | grep -v "mod mixnet;$")" > ./polkadot-sdk/substrate/client/rpc/src/lib.rs
+remove_matching_lines ./polkadot-sdk/substrate/client/rpc/src/lib.rs "mod mixnet;$"
 rm ./polkadot-sdk/substrate/client/cli/src/params/mixnet_params.rs
-echo "$(cat ./polkadot-sdk/substrate/client/cli/src/params/mod.rs | grep -v "mod mixnet_params;$")" > ./polkadot-sdk/substrate/client/cli/src/params/mod.rs
+remove_matching_lines ./polkadot-sdk/substrate/client/cli/src/params/mod.rs "mod mixnet_params;$"
 sed -e s/" mixnet_params::\*,"//g -i ./polkadot-sdk/substrate/client/cli/src/params/mod.rs
 
+# Remove the 'statement store'
 remove_crate_tree substrate/client/network/statement
 remove_crate_tree substrate/client/statement-store
 remove_crate_tree substrate/frame/statement
 remove_crate_tree substrate/primitives/statement-store
 rm -rf ./polkadot-sdk/substrate/client/rpc-api/src/statement
-echo "$(cat ./polkadot-sdk/substrate/client/rpc-api/src/lib.rs | grep -v "mod statement;$")" > ./polkadot-sdk/substrate/client/rpc-api/src/lib.rs
+remove_matching_lines ./polkadot-sdk/substrate/client/rpc-api/src/lib.rs "mod statement;$"
 rm -rf ./polkadot-sdk/substrate/client/rpc/src/statement
-echo "$(cat ./polkadot-sdk/substrate/client/rpc/src/lib.rs | grep -v "mod statement;$")" > ./polkadot-sdk/substrate/client/rpc/src/lib.rs
+remove_matching_lines ./polkadot-sdk/substrate/client/rpc/src/lib.rs "mod statement;$"
 
+# Remove the binary Merkle tree code, as we only use the standard base-16 trie
 remove_crate_tree substrate/utils/binary-merkle-tree
 rm ./polkadot-sdk/substrate/primitives/runtime/src/proving_trie/base2.rs
-echo "$(cat ./polkadot-sdk/substrate/primitives/runtime/src/proving_trie/mod.rs | grep -v "mod base2;$")" > ./polkadot-sdk/substrate/primitives/runtime/src/proving_trie/mod.rs
+remove_matching_lines ./polkadot-sdk/substrate/primitives/runtime/src/proving_trie/mod.rs "mod base2;$"
 apply_patch patches/remove_binary_merkle_tree_prover.patch
 
+# Remove the transaction storage code
 remove_crate_tree substrate/frame/transaction-storage
 remove_crate_tree substrate/primitives/transaction-storage-proof
 apply_patch patches/remove_sp_transaction_storage_proof.patch
 
+# Remove non-Ristretto cryptography
 remove_crate_tree substrate/primitives/crypto/ec-utils
 remove_feature bls-experimental
 rm ./polkadot-sdk/substrate/primitives/core/src/bls.rs
@@ -199,22 +204,28 @@ rm ./polkadot-sdk/substrate/primitives/application-crypto/src/bandersnatch.rs
 rm ./polkadot-sdk/substrate/primitives/core/src/bandersnatch.rs
 rm ./polkadot-sdk/substrate/primitives/keyring/src/bandersnatch.rs
 rm ./polkadot-sdk/substrate/primitives/core/src/paired_crypto.rs
-echo "$(cat ./polkadot-sdk/substrate/primitives/core/src/lib.rs | grep -v "mod paired_crypto;$")" > ./polkadot-sdk/substrate/primitives/core/src/lib.rs
+remove_matching_lines ./polkadot-sdk/substrate/primitives/core/src/lib.rs "mod paired_crypto;$"
 
-remove_crate_tree substrate/frame/bags-list/fuzzer
-remove_crate_tree substrate/frame/bags-list/remote-tests
-remove_crate_tree substrate/frame/election-provider-support/solution-type/fuzzer
-remove_crate_tree substrate/frame/paged-list/fuzzer
-remove_crate_tree substrate/primitives/arithmetic/fuzzer
-remove_crate_tree substrate/primitives/npos-elections/fuzzer
+# TODO rm substrate/primitives/core/src/ecdsa.rs
+# TODO rm substrate/primitives/application-crypto/src/ecdsa.rs
+# TODO rm substrate/primitives/application-crypto/test/src/ecdsa.rs
+# TODO rm substrate/frame/support/src/crypto/ecdsa.rs
 
+# TODO rm substrate/primitives/application-crypto/src/ed25519.rs
+# TODO rm substrate/primitives/application-crypto/test/src/ed25519.rs
+# TODO rm substrate/primitives/core/src/ed25519.rs
+# TODO rm substrate/primitives/keyring/src/ed25519.rs
+
+# Remove the metadata's hash from the runtime
+remove_feature metadata-hash
+rm ./polkadot-sdk/substrate/utils/wasm-builder/src/metadata_hash.rs
+remove_matching_lines ./polkadot-sdk/substrate/test-utils/runtime/build.rs "enable_metadata_hash"
+
+# Remove the extension which checks the metadata's hash from the runtime
 remove_crate_tree substrate/frame/metadata-hash-extension
 apply_patch patches/remove_metadata_hash_extension.patch
 
-remove_feature metadata-hash
-rm ./polkadot-sdk/substrate/utils/wasm-builder/src/metadata_hash.rs
-echo "$(cat ./polkadot-sdk/substrate/test-utils/runtime/build.rs | grep -v 'enable_metadata_hash')" > ./polkadot-sdk/substrate/test-utils/runtime/build.rs
-
+# Remove unused pallets
 remove_crate_tree substrate/frame/alliance
 remove_crate_tree substrate/frame/asset-conversion
 remove_crate_tree substrate/frame/asset-rate
@@ -234,7 +245,6 @@ remove_crate_tree substrate/frame/democracy
 remove_crate_tree substrate/frame/derivatives
 remove_crate_tree substrate/frame/dummy-dim
 remove_crate_tree substrate/frame/elections-phragmen
-remove_crate_tree substrate/frame/examples
 remove_crate_tree substrate/frame/fast-unstake
 remove_crate_tree substrate/frame/lottery
 remove_crate_tree substrate/frame/membership
@@ -257,14 +267,24 @@ remove_crate_tree substrate/frame/transaction-payment/asset-tx-payment
 remove_crate_tree substrate/frame/treasury
 remove_crate_tree substrate/frame/vesting
 
+# Remove the `scripts` used for testing
 remove_crate_tree substrate/scripts
 
+# Remove fuzzers
+remove_crate_tree substrate/frame/bags-list/fuzzer
+remove_crate_tree substrate/frame/bags-list/remote-tests
+remove_crate_tree substrate/frame/election-provider-support/solution-type/fuzzer
+remove_crate_tree substrate/frame/paged-list/fuzzer
+remove_crate_tree substrate/primitives/arithmetic/fuzzer
+remove_crate_tree substrate/primitives/npos-elections/fuzzer
+
+# Remove benchmarking code we don't use
 remove_crate_tree substrate/frame/system/benchmarking
 apply_patch patches/remove_frame_system_benchmarking.patch
-
 remove_crate_tree substrate/utils/frame/benchmarking-cli
 remove_crate_tree substrate/utils/frame/omni-bencher
 
+# Remove all dev dependencies, tests, benches, etc.
 remove_dev_dependencies
 
 # Remove the `SS58prefix` constant
@@ -273,6 +293,7 @@ find ./polkadot-sdk/substrate -iname "*.rs" -exec sh -c "cat {} | grep -v SS58Pr
 
 cd polkadot-sdk
 
+# Perform upgrades to preferred versions
 cargo +nightly update -Z unstable-options --breaking -p twox-hash --precise 2.1.1
 cargo +nightly update -Z unstable-options --breaking -p hex-literal --precise 1.0.0
 cargo +nightly update -Z unstable-options --breaking -p thiserror --precise 2.0.16
@@ -303,18 +324,19 @@ rm .prdoc.toml
 rm Plan.toml
 
 # Restore the committed `Cargo.lock` so this is deterministic, if one exists
+rm ./Cargo.lock || true
 cp ../Cargo.lock.polkadot-sdk ./Cargo.lock || true
 
 # Ensure this worked as expected
 echo "Running \`cargo check\`"
-cargo +1.88 check --all-features
+cargo check --all-features
 if [ $? -ne 0 ]; then
   echo "Patched \`polkadot-sdk\` failed to compile"
   exit 4
 fi
 
 # Save >10 GB on what should be a static directory of no further use
-# cargo clean
+cargo clean
 
 touch .patched
 
@@ -322,24 +344,8 @@ cd ..
 
 echo "Patched"
 
-# TODO rm substrate/primitives/core/src/ecdsa.rs
-# TODO rm substrate/primitives/application-crypto/src/ecdsa.rs
-# TODO rm substrate/primitives/application-crypto/test/src/ecdsa.rs
-
-# TODO rm substrate/primitives/application-crypto/src/ed25519.rs
-# TODO rm substrate/primitives/application-crypto/test/src/ed25519.rs
-# TODO rm substrate/primitives/core/src/ed25519.rs
-# TODO rm substrate/primitives/keyring/src/ed25519.rs
-
 # TODO rm substrate/client/offchain/src/api/http.rs
-# TODO rm substrate/frame/support/procedural/src/pallet/expand/constants.rs
-# TODO rm substrate/frame/support/procedural/src/pallet/expand/documentation.rs
-# TODO rm substrate/frame/support/src/crypto/ecdsa.rs
-# TODO rm substrate/frame/support/src/traits/proving.rs
-# TODO rm substrate/frame/support/src/traits/voting.rs
-# TODO rm substrate/primitives/core/src/paired_crypto.rs
+# TODO rm substrate/primitives/runtime/src/offchain/http.rs
 # TODO rm substrate/primitives/metadata-ir/src/unstable.rs
 # TODO rm substrate/primitives/metadata-ir/src/v14.rs
 # TODO rm substrate/primitives/metadata-ir/src/v15.rs
-# TODO rm substrate/primitives/runtime/src/offchain/http.rs
-# TODO rm substrate/utils/wasm-builder/src/metadata_hash.rs
